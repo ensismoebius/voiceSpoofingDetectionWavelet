e7e136b48f3a99037e176ccd55b1b9bfb05005e7
Andre Furlan <ensismoebius@gmail.com>
2020-03-27 01:05:30 -0300

    Dia06 Após 3 dias estudando as caracteristicas das wavelets chegamos a Wavelet de Morlet
    Ssegundo a fonte que estou estudando agora deve ter a mesma frequência do sinal a ser estudado. Isso já foi um bom avanço pois agora tenho arquivos que, em tese, implementam wavelets em matlab disponivel para testes e comparação.
    
    Pendências
    Essa parte da frequência da wavelet para mim é nova preciso tirar dúvidas sobre com o orientador. Preciso também experimentar com o código que tenho para ver se consigo alguma coisa sozinho.

--- pesquisa/day03/scalingAndTranslatingFunctions.m
+++ pesquisa/day03/scalingAndTranslatingFunctions.m
@@ -1,17 +1,14 @@
-xaxis = [-1000*pi : 1000*pi];
+xaxis = [-100*pi : 100*pi];
 
 scales = [ 1,2,4,8 ];
-translations = [ 1,2,3,4,5 ];
+%translations = [ 1,2,3,4,5 ];
+translations = [ 1:1:5 ];
 
-%hold on;
 for scale = scales
   for translation = translations
-    plot(waveletScaleAndTransFunction(xaxis,scale,translation,@sin));
+    plot(waveletScaleAndTransFunction(xaxis,scale,translation,@waveletMexicanFunction));
     title( sprintf('Scale: %d - Translation: %d', scale, translation));
     grid off;
-    %print('figure.pdf','-dpdf')
+    pause (.0001);
   endfor
 endfor
-
-%hold off;
-
