72749ba0abf00fc58f3f0a6ac86b3755077b77bc
Andre Furlan <ensismoebius@gmail.com>
2020-04-29 13:50:34 -0300

    Todos os áudios foram separados

--- rename.sh
+++ rename.sh
@@ -1,9 +1,11 @@
-t=$(ls soundSamples/separated/playback/Voz?/*/? | sed -z 's/:\n/\//g' | sed -z 's/\n\n/\n/g')
+t=$(ls soundSamples/separated/playback/Voz??/*/? | sed -z 's/:\n/\//g' | sed -z 's/\n\n/\n/g')
 for file in $t
 do
 	fileName=$(echo $file | cut -d'/' -f4); 
 	lang=$(echo $file | cut -d'/' -f5); 
 	digit=$(echo $file | cut -d'/' -f6); 
-	echo $file
-	echo soundSamples/separated/playback/$lang/$digit/$fileName.wav
+
+	mkdir -p soundSamples/separated/playback/$lang/$digit
+
+	mv $file soundSamples/separated/playback/$lang/$digit/$fileName.wav
 done
