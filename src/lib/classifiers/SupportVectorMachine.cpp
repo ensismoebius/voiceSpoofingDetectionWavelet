b54d6ce0f65641b628c6d232b397458e5a952899
Andre Furlan <ensismoebius@gmail.com>
2020-05-14 04:13:52 -0300

    Alguns comentários e otimizações

--- src/lib/classifiers/SupportVectorMachine.cpp
+++ src/lib/classifiers/SupportVectorMachine.cpp
@@ -18,48 +18,87 @@
 
 namespace classifiers {
 
+	/**
+	 * A support vector machine implementation
+	 */
 	class SupportVectorMachine {
 		public:
+
+			/**
+			 * Labels for data
+			 */
 			enum LABEL {
 				POSITIVE = 1, NEGATIVE = -1
 			};
 		private:
 
-			std::vector<std::vector<double>> trainningModels;
+			/**
+			 * Labels to the trainning models
+			 */
 			std::vector<LABEL> trainningLabels;
 
-			std::vector<double> hiddenToOutputWeights;
-			std::vector<double> outputLayer;
+			/**
+			 * This is the reference for trainning 
+			 * and classification of further data
+			 */
+			std::vector<std::vector<double>> trainningModels;
 
+			/**
+			 * Holds the weights used for classification
+			 */
+			std::vector<double> outputWeights;
+
+			/**
+			 * Used to calculate the radial distances between two vectors
+			 * @param currentInputVector
+			 * @param v2
+			 * @return distance
+			 */
 			inline double radialDistance(std::vector<double> currentInputVector, std::vector<double> v2) {
 				return std::exp(-euclidianDistance(currentInputVector, v2));
 			}
 
 		public:
 
-			void addTrainningCases(std::vector<std::vector<double>> input, LABEL label) {
-				this->trainningLabels.resize(this->trainningLabels.size() + input.size(), label);
-				this->trainningModels.insert(this->trainningModels.end(), input.begin(), input.end());
+			/**
+			 * Add trainning matrices to SVM
+			 * @param matrix - A matrix (2d vector) with trainning cases 
+			 * @param label - Label indicating if it is positive or negative sample
+			 */
+			void addTrainningCases(std::vector<std::vector<double>> matrix, LABEL label) {
+				this->trainningLabels.resize(this->trainningLabels.size() + matrix.size(), label);
+				this->trainningModels.insert(this->trainningModels.end(), matrix.begin(), matrix.end());
 			}
 
+			/**
+			 * Do the classification!!!
+			 * @param input - The vector to be tested
+			 * @return POSITIVE or NEGATIVE
+			 */
 			LABEL evaluate(std::vector<double> input) {
 
-				double result = 0;
+				// Weighted distance from input and trainning models
+				double wDistance = 0;
 
 				// creating the layer values
 				for (unsigned int fi = 0; fi < this->trainningModels.size(); fi++) {
-					result += this->radialDistance(input, this->trainningModels[fi]) * this->hiddenToOutputWeights[fi];
+					wDistance += this->outputWeights[fi] * this->radialDistance(input, this->trainningModels[fi]);
 				}
 
 				// If the distance from NEGATIVE is greater
 				// than the distance from POSITIVE return
 				// POSITIVE otherwise return NEGATIVE
-				if (std::pow(result - NEGATIVE, 2) > std::pow(result - POSITIVE, 2)) {
+				if (std::pow(wDistance - NEGATIVE, 2) > std::pow(wDistance - POSITIVE, 2)) {
 					return POSITIVE;
 				}
 				return NEGATIVE;
 			}
 
+			/**
+			 * Trainning method, use this after use
+			 * @addTrainningCases method
+			 * @see addTrainningCases
+			 */
 			void train() {
 				// Used to determine the weights for the connections to the output
 				std::vector<std::vector<double>> distancesMatrix;
@@ -86,7 +125,7 @@ namespace classifiers {
 				linearAlgebra::scaleMatrix(distancesMatrix);
 
 				// system solved!!! Return the coeficcients
-				this->hiddenToOutputWeights = linearAlgebra::solveMatrix(distancesMatrix);
+				this->outputWeights = linearAlgebra::solveMatrix(distancesMatrix);
 			}
 	};
 }
