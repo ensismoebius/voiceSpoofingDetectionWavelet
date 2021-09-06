14cf5abe63b97ba564f3843eecf8533bcb3c2241
Andre Furlan <ensismoebius@gmail.com>
2021-09-06 17:36:25 -0300

    Última modificação feita antes do envio para correção do orientador

--- src/lib/paraconsistent/paraconsistent.h
+++ src/lib/paraconsistent/paraconsistent.h
@@ -12,11 +12,54 @@
 #include <limits>
 #include <vector>
 
+/**
+ * @brief Given the alfa and the betha calculates the certainty degree
+ * 
+ * @param alpha 
+ * @param betha 
+ * @return long double 
+ */
 long double calcCertaintyDegree_G1(long double alpha, long double betha);
+
+/**
+ * @brief Given the alfa and the betha calculates the contradiction degree
+ * 
+ * @param alpha 
+ * @param betha 
+ * @return long double 
+ */
 long double calcContradictionDegree_G2(long double alpha, long double betha);
 
+/**
+ * @brief Calculates the alpha value
+ * 
+ * @param amountOfClasses Amount of classes
+ * @param featureVectorsPerClass Amount of feature vectors per class
+ * @param featureVectorSize Size of the feature vectors
+ * @param arrClasses Holds all classes
+ * @return long double Value of alpha
+ */
 long double calculateAlpha(unsigned int amountOfClasses, unsigned int featureVectorsPerClass, unsigned int featureVectorSize, std::map<std::string, std::vector<std::vector<long double>>> &arrClasses);
+
+/**
+ * @brief Calculates the beta value
+ * 
+ * @param amountOfClasses Amount of classes
+ * @param featureVectorsPerClass Amount of feature vectors per class
+ * @param featureVectorSize Size of the feature vectors
+ * @param arrClasses Holds all classes
+ * @return long double Value of beta
+ */
 long double calculateBeta(unsigned int amountOfClasses, unsigned int featureVectorsPerClass, unsigned int featureVectorSize, std::map<std::string, std::vector<std::vector<long double>>> &arrClasses);
+
+/**
+ * @brief Normalize all feature vectors from all classes
+ * 
+ * @param amountOfClasses Amount of classes
+ * @param featureVectorsPerClass Amount of feature vectors per class
+ * @param featureVectorSize Size of the feature vectors
+ * @param arrClasses Holds all classes
+ */
 void normalizeClassesFeatureVectors(unsigned int amountOfClasses, unsigned int featureVectorsPerClass, unsigned int featureVectorSize, std::map<std::string, std::vector<std::vector<long double>>> &arrClasses);
 
 #endif /* LIB_PARACONSISTENT_H_ */
